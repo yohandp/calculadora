ff987891bbdc46a406c3ddec794754e53e3ce93f
yohandp <yohandpessanha@gmail.com>
2022-03-25 09:54:19 -0300

    Adicionando classe calculadora que recebera a operacao desejada pelo usuario

---
+++ calculadora/Calculadora.java
@@ -0,0 +1,11 @@
+package calculadora;
+import java.util.Scanner;
+public class Calculadora{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite a operacao desejada: \t 1->Soma \t 2->Subtracao \t 3->Multiplicacao \t 4->Divisao");
+        int operacao = scan.nextInt();
+
+        //switch operacoes
+    }
+}
