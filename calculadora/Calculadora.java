0b89f9f9058c7e44e8d6de5d26356264e17d009d
yohandp <yohandpessanha@gmail.com>
2022-03-25 09:59:03 -0300

    Finalizando implementacao classe calculadora com switch case para operacoes realizadas

--- calculadora/Calculadora.java
+++ calculadora/Calculadora.java
@@ -1,11 +1,29 @@
 package calculadora;
 import java.util.Scanner;
 public class Calculadora{
-    public static void main(String[] args) {
+    static public void main(String[] args){
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Digite a operacao desejada: \t 1->Soma \t 2->Subtracao \t 3->Multiplicacao \t 4->Divisao");
         int operacao = scan.nextInt();
 
-        //switch operacoes
+        switch (operacao){
+            case 1:
+                OperacoesCalc soma = new OperacoesCalc();
+                soma.soma();
+                break;
+            case 2:
+                OperacoesCalc subtracao = new OperacoesCalc();
+                subtracao.subtracao();
+                break;
+            case 3:
+                OperacoesCalc multiplicacao = new OperacoesCalc();
+                multiplicacao.multiplicacao();
+                break;
+            case 4:
+                OperacoesCalc divisao = new OperacoesCalc();
+                divisao.divisao();
+                break;
+        }
     }
 }
